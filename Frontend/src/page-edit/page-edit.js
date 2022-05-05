e1014cc8e7e0c4327e0f54aec6bdeeafbd86d899
Melissa <meli.sch09@gmail.com>
2022-05-05 16:22:01 +0200

    Fehler anpassen

--- Frontend/src/page-edit/page-edit.js
+++ Frontend/src/page-edit/page-edit.js
@@ -4,8 +4,8 @@ import Page from "../page.js";
 import HtmlTemplate from "./page-edit.html";
 
 /**
- * Klasse PageEdit: Stellt die Seite zum Anlegen oder Bearbeiten einer Adresse
- * zur Verfügung.
+ * Klasse PageEdit: Stellt die Seite zum Anlegen oder Bearbeiten eines Kleintieres
+  * zur Verfügung.
  */
 export default class PageEdit extends Page {
     /**
@@ -36,28 +36,14 @@ export default class PageEdit extends Page {
         this._rasseInput     = null;
     }
 
-    /**
-     * HTML-Inhalt und anzuzeigende Daten laden.
-     *
-     * HINWEIS: Durch die geerbte init()-Methode wird `this._mainElement` mit
-     * dem <main>-Element aus der nachgeladenen HTML-Datei versorgt. Dieses
-     * Element wird dann auch von der App-Klasse verwendet, um die Seite
-     * anzuzeigen. Hier muss daher einfach mit dem üblichen DOM-Methoden
-     * `this._mainElement` nachbearbeitet werden, um die angezeigten Inhalte
-     * zu beeinflussen.
-     *
-     * HINWEIS: In dieser Version der App wird mit dem üblichen DOM-Methoden
-     * gearbeitet, um den finalen HTML-Code der Seite zu generieren. In größeren
-     * Apps würde man ggf. eine Template Engine wie z.B. Nunjucks integrieren
-     * und den JavaScript-Code dadurch deutlich vereinfachen.
-     */
+  
     async init() {
         // HTML-Inhalt nachladen
         await super.init();
 
         // Bearbeiteten Datensatz laden
         if (this._editId) {
-            this._url = `/address/${this._editId}`;
+            this._url = `/kleintiere/${this._editId}`;
             this._dataset = await this._app.backend.fetch("GET", this._url);
             this._title = `${this._dataset.name} ${this._dataset.rasse}`;
         } else {
