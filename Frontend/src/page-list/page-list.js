8db04fbb280ab3ecb32c6174beeec87fa39b1b34
Melissa <meli.sch09@gmail.com>
2022-05-06 15:02:30 +0200

    Fehler behoben

--- Frontend/src/page-list/page-list.js
+++ Frontend/src/page-list/page-list.js
@@ -18,7 +18,7 @@ export default class PageList extends Page {
         this._emptyMessageElement = null;
     }
 
-    
+
     async init() {
         // HTML-Inhalt nachladen
         await super.init();
@@ -59,7 +59,7 @@ export default class PageList extends Page {
             olElement.appendChild(liElement);
 
             // Event Handler registrieren
-            liElement.querySelector(".action.edit").addEventListener("click", () => location.hash = `#/edit/${dataset._id}`);
+            liElement.querySelector(".action.edit").addEventListener("click", () => location.hash = `#/editT/${dataset._id}`);
             liElement.querySelector(".action.delete").addEventListener("click", () => this._askDelete(dataset._id));
         }
     }
